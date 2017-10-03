c30437c022a0d76ceeb1bfc3a6e4bade250a2b8b
kitty <158086780@qq.com>
2017-10-03 09:57:23 +0800

    增加通告审核页面配置

--- config/config.js
+++ config/config.js
@@ -24,7 +24,11 @@ var daBai ={};
 
     daBai.Config.map = [
         { path: '/', view: 'index.html', viewUrl: 'index.html', styles: null, scripts: 'appindex', modules: 'appindex' },
-        { path: '/index/resetPassword', view: 'index/resetPassword.html', viewUrl: 'index/resetPassword.html', styles: null, scripts: 'index/resetPassword', modules: 'resetPassword' }
+        { path: '/index/resetPassword', view: 'index/resetPassword.html', viewUrl: 'index/resetPassword.html', styles: null, scripts: 'index/resetPassword', modules: 'resetPassword' },
+        { path: '/overallPlanning/announcementAudit', view: 'overallPlanning/announcementAudit.html', viewUrl: 'overallPlanning/announcementAudit.html', styles: null, scripts: 'overallPlanning/announcementAudit', modules: 'announcementAudit' },
+        { path: '/overallPlanning/arrangementNotice', view: 'overallPlanning/arrangementNotice.html', viewUrl: 'overallPlanning/arrangementNotice.html', styles: null, scripts: 'overallPlanning/arrangementNotice', modules: 'arrangementNotice' },
+        { path: '/overallPlanning/confirm', view: 'overallPlanning/confirm.html', viewUrl: 'overallPlanning/confirm.html', styles: null, scripts: 'overallPlanning/confirm', modules: 'overallPlanningConfirm' },
+        { path: '/overallPlanning/progressAdmin', view: 'overallPlanning/progressAdmin.html', viewUrl: 'overallPlanning/progressAdmin.html', styles: null, scripts: 'overallPlanning/progressAdmin', modules: 'progressAdmin' }
 
     ];
 
